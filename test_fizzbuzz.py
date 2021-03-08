0572930f22bdbb45b1cd2ec5c7237f4f52b9005b
rodartes <rodartes@oregonstate.edu>
2021-03-08 02:56:51 +0000

    wrote more tests for fizzbuzz checking several cases of each output value

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,13 +1,21 @@
 import fizzbuzz
 
 def test_fizz():
-   assert fizzbuzz.fizzbuzz(3) == "Fizz"
+  three_muls = [3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51]
+  for x in three_muls:
+    assert fizzbuzz.fizzbuzz(x) == "Fizz"
 
 def test_buzz():
-  assert fizzbuzz.fizzbuzz(5) == "Buzz"
+  five_muls = [5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100]
+  for x in five_muls:
+   assert fizzbuzz.fizzbuzz(x) == "Buzz"
 
 def test_fizzbuzz():
-  assert fizzbuzz.fizzbuzz(15) == "FizzBuzz"
+  three_five_muls = [15, 30, 45, 60, 75, 90]
+  for x in three_five_muls:
+    assert fizzbuzz.fizzbuzz(x) == "FizzBuzz"
 
 def test_nofizzbuzz():
-  assert fizzbuzz.fizzbuzz(1) == 1
+  non_muls = [1, 2, 4, 7, 11, 13, 14, 16, 17, 19, 22, 23, 26, 28, 29,31]
+  for x in non_muls:
+    assert fizzbuzz.fizzbuzz(x) == x
